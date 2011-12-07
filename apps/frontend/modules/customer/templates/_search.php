3015e4aaa428faaecb2e3473e55b8230ad577c74
Florian Engler <florian.engler@gmx.de>
2011-12-07 17:22:20 +0100

    Customer Frontend

---
+++ apps/frontend/modules/customer/templates/_search.php
@@ -0,0 +1,9 @@
+<?php use_javascript('/sfFormExtraPlugin/js/jquery.autocompleter.js') ?>
+<?php use_stylesheet('/sfFormExtraPlugin/css/jquery.autocompleter.css') ?>
+
+<form action="<?php echo url_for('customer/search') ?>" method="get" accept-charset="utf-8">
+	<?php echo $form ?>
+
+	<input type="submit" value="Anzeigen">
+</form>
+
